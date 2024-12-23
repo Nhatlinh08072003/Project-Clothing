113a9348467bda75dbbe55e88177ad575c9fa89a
Nhatlinh08072003 <letiennhatlinh08072003@gmail.com>
2024-12-23 08:04:22 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */  
+module.exports = {  
+  content: [  
+    "./Pages/**/*.{cshtml,html}",  
+    "./Views/**/*.{cshtml,html}",  
+    "./wwwroot/**/*.{js,html}",  
+    // Thêm bất kỳ tệp nào khác mà bạn sử dụng Tailwind CSS  
+  ],  
+  theme: {  
+    extend: {},  
+  },  
+  plugins: [],  
+}
